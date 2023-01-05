b2648dca7125ace576f132ac54debdb2d534f974
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-01-05 16:35:42 +0530

    Create 5th_January.cpp

---
+++ 5th_January.cpp
@@ -0,0 +1,45 @@
+//User function Template for C++
+
+class Solution
+{
+public:
+    string longestString(vector<string> &words)
+    {
+        // code here
+        unordered_map<string,int> m;
+        
+        sort(words.begin(), words.end());
+
+        for(auto i:words) m[i]++;
+
+        string ans;
+
+        int size=INT_MIN;
+
+        for(auto i:words){
+
+            string temp;
+
+            int c= 0, n=i.size();
+
+            for(int j=0;j<n;j++){
+
+                temp+=i[j];
+
+                if(m[temp]) c++;
+
+            }
+
+            if(c==n && n>size){
+
+                size=n;
+
+                ans=i;
+
+            }
+
+        }
+
+        return ans;
+    }
+};
