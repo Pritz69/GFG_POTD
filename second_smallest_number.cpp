19a3300b2f8454ea7b12d6da95f68cb975289d90
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-03-16 12:46:00 +0530

    Create second_smallest_number.cpp

---
+++ second_smallest_number.cpp
@@ -0,0 +1,46 @@
+//User function Template for C++
+
+class Solution{   
+  public:
+    string secondSmallest(int S, int D){
+        // code here
+        if(S>=D*9)return "-1";
+        
+        string ans="";
+        S-=1;
+        for(int i=D-1; i>0; i--)
+        {
+            if(S>9)
+            {
+                ans+='9';
+                S-=9;
+            }
+            else
+            {
+                ans+=(S+'0');
+                S=0;
+            }
+        }
+        
+        ans+=(S+1+'0');
+        reverse(ans.begin(),ans.end());
+        
+        int index=D-1;
+        for(int i=D-1; i>=0; i--)
+        {
+            if(ans[i]=='9')
+            {
+                index=i;
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        ans[index] -= 1;
+        ans[index-1] += 1;
+        
+        return ans;
+    }
+};
