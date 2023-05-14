8e7d337cad5fa6d59728433c6c94b3221d81e4c1
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-05-14 22:36:06 +0530

    Create Maximum_Subset_Sum.cpp

---
+++ Maximum_Subset_Sum.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class IntArray
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    long long findMaxSubsetSum(int N, vector<int> &A) {
+        // code here
+        long long sum = 0;
+        long long taken = 0;
+        long long notTaken = 0;
+        for(int i = 0; i < N; i++){
+            int temp = taken;
+            taken = max(taken, notTaken) + A[i];
+            notTaken = temp;
+        }
+        return max(taken, notTaken);
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        IntArray::input(A,N);
+        
+        Solution obj;
+        long long res = obj.findMaxSubsetSum(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
