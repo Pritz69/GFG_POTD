9ac56a947d5fa6bb56de825e1ef0e1593dc4e389
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-06-11 21:34:09 +0530

    Create Adding_Ones.py

---
+++ Adding_Ones.py
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+    void update(int a[], int n, int updates[], int k)
+    {
+        // Your code goes here
+        for(int i = 0; i < k; i++)
+            a[updates[i]-1]++;
+        for(int i = 1; i < n; i++){
+            a[i] += a[i - 1];
+        }
+    }
+};
