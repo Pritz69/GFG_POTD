1a669360ec2c87a5472ccdcd860fde8069ca2fca
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-07-19 23:58:02 +0530

    Create Longest_Palindromic_Subsequence.java

---
+++ Longest_Palindromic_Subsequence.java
@@ -0,0 +1,42 @@
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int lcs(String s1,String s2)
+    {
+        int n=s1.length();
+        int m=s2.length();
+        int [][]dp=new int[n+1][m+1];
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(i==0||j==0)
+                {
+                    dp[i][j]=0;
+                }
+                else if(s1.charAt(i-1) == s2.charAt(j-1))
+                {
+                    dp[i][j]= 1 + dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]= Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    public int longestPalinSubseq(String S)
+    {
+        //code here
+        StringBuilder sb=new StringBuilder();
+        sb.append(S);
+        sb.reverse();
+        String x=sb.toString();
+        int ans=lcs(S,x);
+        return ans;
+    }
+}
