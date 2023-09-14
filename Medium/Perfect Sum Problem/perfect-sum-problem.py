6bee9218df13d86b357fac7d18835ff2bf7ffec2
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-09-15 00:27:48 +0530

    Update perfect-sum-problem.py

--- Medium/Perfect Sum Problem/perfect-sum-problem.py
+++ Medium/Perfect Sum Problem/perfect-sum-problem.py
@@ -1,18 +1,25 @@
 #User function Template for python3
 class Solution:
-	def perfectSum(self, arr, n, sum):
-		# code here
-        mod=10**9+7
-        dp=[[0]*(sum+1) for _ in range(n+1)]
-        dp[0][0]=1
-        for i in range(1,n+1):
-            for j in range(sum+1):
-                if arr[i-1]>j:
-                    dp[i][j]=dp[i-1][j]
-                else:
-                    dp[i][j]=(dp[i-1][j]+dp[i-1][j-arr[i-1]])%mod
-        return dp[n][sum]
-
+    def perfectSum(self, arr, n, sum):
+        # code here
+        dp=dict()
+        mod=10**9 + 7
+        def rec(i,tar) : 
+            if i==n :
+                if tar==0 :
+                    return 1
+                else :
+                    return 0         
+            if (i,tar) in dp :
+                return dp[(i,tar)]  
+            tk=0
+            if (arr[i] <= tar) :
+                tk= rec(i+1,tar-arr[i]) % mod
+                
+            ntk= rec(i+1,tar) % mod
+            dp[(i,tar)] = (tk + ntk) % mod 
+            return dp[(i,tar)]
+        return rec(0,sum)
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
