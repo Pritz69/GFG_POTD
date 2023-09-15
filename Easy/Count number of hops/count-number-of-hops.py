547166edd69c0f7778da8bfe738c6580d5153d04
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-09-16 00:50:15 +0530

    Update count-number-of-hops.py

--- Easy/Count number of hops/count-number-of-hops.py
+++ Easy/Count number of hops/count-number-of-hops.py
@@ -1,3 +1,23 @@
+#Tabulation 
+#User function Template for python3
+
+class Solution:
+    #Function to count the number of ways in which frog can reach the top.
+    def countWays(self,n):
+        dp=[0]*(n+1)
+        mod=10**9 + 7
+        dp[n]=1
+        for i in range(n-1,-1,-1) :
+            if i+1 <=n :
+                dp[i]+=dp[i+1]
+            if i+2 <=n :
+                dp[i]+=dp[i+2]
+            if i+3<=n :
+                dp[i]+=dp[i+3]
+            dp[i]=dp[i]%mod
+        return dp[0]
+
+
 #User function Template for python3
 
 class Solution:
