02a4a63443491654586d476d16057134376af5d0
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-09-18 19:27:19 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Power of 2/power-of-2.py
@@ -0,0 +1,43 @@
+#User function Template for python3
+
+class Solution:
+    ##Complete this function
+    # Function to check if given number n is a power of two.
+    def isPowerofTwo(self,n):
+        ##Your code here
+        if n==1 :
+            return True
+        while n >1 :
+            n=n/2
+        if n==1 :
+            return True
+        else :
+            return False
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        ob=Solution()
+        if ob.isPowerofTwo(n):
+            print("YES")
+        else:
+            print("NO")
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
