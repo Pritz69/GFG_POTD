10e2b9dc6d041cd284a6b80f6c5fb12cd48575dc
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-09-19 21:19:11 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find first set bit/find-first-set-bit.py
@@ -0,0 +1,53 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find position of first set bit in the given number.
+    def getFirstSetBit(self,n):
+        #Your code here
+        if n==0 :
+            return 0
+        c=0
+        while n >0 :
+            if n%2==1 :
+                break
+            c +=1
+            n = n//2
+        return c+1
+
+#{ 
+ # Driver Code Starts.
+    
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        ob=Solution()
+        
+        print(ob.getFirstSetBit(n))
+        
+        
+        T-=1
+    
+    
+
+
+
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
