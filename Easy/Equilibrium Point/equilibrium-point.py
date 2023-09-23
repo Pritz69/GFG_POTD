d475d154dc8523ab73a7e56c1fa8bb869eaaafe5
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-09-23 16:40:22 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Equilibrium Point/equilibrium-point.py
@@ -0,0 +1,44 @@
+# User function Template for python3
+
+class Solution:
+    # Complete this function
+    
+    #Function to find equilibrium point in the array.
+    def equilibriumPoint(self,A, N):
+        # Your code here
+        ans=-1
+        s=sum(A)
+        ps=0
+        for i in range(N) :
+            if ps==(s-ps-A[i]) :
+                ans=i+1
+            ps +=A[i]
+        return ans
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+import math
+
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+
+        N = int(input())
+
+        A = [int(x) for x in input().strip().split()]
+        ob=Solution()
+
+        print(ob.equilibriumPoint(A, N))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
