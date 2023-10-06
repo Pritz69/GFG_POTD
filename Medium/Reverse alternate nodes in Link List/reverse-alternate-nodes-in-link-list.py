f8a15fd8d81e8bc4565649a2cfc2afaa6c4e304c
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-10-06 13:43:30 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Reverse alternate nodes in Link List/reverse-alternate-nodes-in-link-list.py
@@ -0,0 +1,89 @@
+"""
+  reverse alternate nodes and append at the end
+  The input list will have at least one element
+  Node is defined as
+class Node:
+
+	# Constructor to initialize the node object
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+"""
+class Solution:
+    def rearrange(self, head):
+        # Code here
+        def reverse(head) :
+            prev=None
+            curr=head
+            while curr :
+                nxt = curr.next
+                curr.next = prev
+                prev=curr
+                curr=nxt
+            return prev
+            
+        odd=head
+        even=head.next
+        evenhead=head.next
+        
+        while even and even.next :
+            odd.next=odd.next.next
+            even.next=even.next.next
+            odd=odd.next
+            even=even.next
+        
+        revevenhead = reverse(evenhead)
+        odd.next = revevenhead
+        
+        return head
+        
+        
+        
+                
+
+#{ 
+ # Driver Code Starts
+# Node Class
+class Node:
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+# Linked list class contains node object
+class LinkedList:
+    # Constructor to initialize head
+    def __init__(self):
+        self.head = None
+
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+        print()
+
+
+# Code execution starts here
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in reversed(values):
+            llist.push(i)
+            
+        Solution().rearrange(llist.head)
+        llist.printList()
+        t -= 1
+# Contributed by: Harshit Sidhwa
+# } Driver Code Ends
