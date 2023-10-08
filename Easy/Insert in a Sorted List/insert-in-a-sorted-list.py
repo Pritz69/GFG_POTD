e6c6a970fc24fcda1207634f4be10b364b1312ce
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-10-08 22:10:39 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insert in a Sorted List/insert-in-a-sorted-list.py
@@ -0,0 +1,73 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+        
+# Linked List Class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.prev=self.head
+    
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.prev = self.head
+        else:
+            self.prev.next = new_node
+            self.prev = self.prev.next
+
+def printList(head):
+    while head:
+        print(head.data, end=' ')
+        head = head.next
+    print()
+
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def sortedInsert(self, head1,key):
+        # code here
+        # return head of edited linked list
+        ins=Node(key)
+        if key < head1.data :
+            ins.next=head1
+            return ins
+        curr=head1
+
+        while curr.next and key > curr.next.data:
+            curr=curr.next
+        
+        tmp=curr.next
+        curr.next=ins
+        ins.next=tmp
+        
+        return head1
+            
+        
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        n = int(input())
+        
+        a = LinkedList()
+        nodes = list(map(int, input().strip().split()))
+        for x in nodes:
+            a.append(x)
+        
+        key=int(input())
+        h=Solution().sortedInsert(a.head,key)
+        printList(h)
+
+# } Driver Code Ends
