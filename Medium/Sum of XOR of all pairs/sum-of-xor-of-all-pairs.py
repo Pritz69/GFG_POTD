33c1f7c919b6e84035643683bf7e25d639f0b3a0
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-10-30 12:36:51 +0530

    Updated Solution - GfG to GitHub

--- Medium/Sum of XOR of all pairs/sum-of-xor-of-all-pairs.py
+++ Medium/Sum of XOR of all pairs/sum-of-xor-of-all-pairs.py
@@ -2,14 +2,31 @@
 
 class Solution:
     def sumXOR (self, arr, n) : 
-        s=0
-        for i in range(0,32):
-            z=0
-            for j in range(n):
-                if(arr[j]&(1<<i))!=0:
-                    z+=1
-            s+=((n-z)*z*(1<<i))
-        return s
+        su = 0
+        for i in range(0, 32):
+     
+            #  Count of zeros and ones
+            zc = 0
+            oc = 0
+              
+            # Individual sum at each bit position
+            idsum = 0
+            for j in range(0, n):
+                if (arr[j] % 2 == 0):
+                    zc = zc + 1
+                     
+                else:
+                    oc = oc + 1
+                arr[j] = arr[j] // 2
+             
+              
+            # calculating individual bit sum 
+            idsum = oc * zc * (1 << i)
+      
+            # final sum    
+            su = su + idsum; 
+         
+        return su
 
 
 #{ 
