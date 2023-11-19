b6a4e29ed6902bcf520e3ea01dbf393f336be3e7
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-11-19 23:15:55 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Intersection of two sorted Linked lists/intersection-of-two-sorted-linked-lists.py
@@ -0,0 +1,79 @@
+#User function Template for python3
+
+''' structure of node:
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+'''
+
+class Solution:
+    def findIntersection(self,head1,head2):
+        d={}
+        while head1:
+            d[head1.data]=d.get(head1.data,0)+1
+            head1=head1.next
+        head=None
+        tail=None
+        while head2:
+            if (head2.data in d) and d[head2.data]:
+                if head is None:
+                    head=Node(head2.data)
+                    tail=head
+                else:
+                    tail.next=Node(head2.data)
+                    tail=tail.next
+                d[head2.data]-=1
+            head2=head2.next
+        return head
+        
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class linkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    
+    def insert(self,data):
+        if self.head is None:
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(data)
+            self.tail = self.tail.next
+
+def printList(head):
+    while head:
+        print(head.data,end=' ')
+        head=head.next
+        
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n1,n2 = [int(x) for x in input().split()]
+        arr1 = [int(x) for x in input().split()]
+        ll1 = linkedList()
+        for i in arr1:
+            ll1.insert(i)
+        
+        arr2 = [int(x) for x in input().split()]
+        ll2 = linkedList()
+        for i in arr2:
+            ll2.insert(i)
+        ob = Solution()
+        result = ob.findIntersection(ll1.head,ll2.head)
+        printList(result)
+        print()
+
+# } Driver Code Ends
