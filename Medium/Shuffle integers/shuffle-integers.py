acc7897530d1c8be26ec88451abf2d97afab8739
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-11-26 01:19:08 +0530

    Updated Solution - GfG to GitHub

--- Medium/Shuffle integers/shuffle-integers.py
+++ Medium/Shuffle integers/shuffle-integers.py
@@ -1,11 +1,12 @@
 class Solution:
     def shuffleArray(self, arr, n):
         # Your code goes here
-        ar1 = arr[n//2:]
-        for i in range(n//2):
-            arr.insert(2*i+1,ar1[i])
-            arr.pop(-1)
-        return arr
+        a = 1
+        b = n//2
+        while b < n:
+            arr.insert(a,arr.pop(b))
+            a += 2 
+            b += 1
 
 #{ 
  # Driver Code Starts
