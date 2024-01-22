cd48e39643ef35bff6f79ba02d6c159e49b3ef06
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-01-22 19:12:36 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/BFS of graph/bfs-of-graph.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        Queue<Integer>q=new LinkedList<>();
+        ArrayList<Integer>arr=new ArrayList<>();
+        int vis[]=new int[V+1];
+        q.offer(0);
+        vis[0]=1;
+        while (!q.isEmpty())
+        {
+            int x=q.poll();
+            arr.add(x);
+            for(int n:adj.get(x))
+            {
+                if (vis[n] != 1)
+                {
+                    vis[n]=1;
+                    q.offer(n);
+                }
+            }
+        }
+        return arr;
+    }
+}
