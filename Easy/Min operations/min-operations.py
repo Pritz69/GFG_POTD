b49698f5d7f49de7168bc129d0f512adff4f211f
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-02-16 20:21:44 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Min operations/min-operations.py
@@ -0,0 +1,30 @@
+class Solution:
+    def solve(self, a : int, b : int) -> int:
+        # code here
+        if(a==b) :
+            return 0
+        if((a&b)==a or (a&b)==b) :
+            return 1
+        return 2
+        
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        a = int(input())
+        
+        
+        b = int(input())
+        
+        obj = Solution()
+        res = obj.solve(a, b)
+        
+        print(res)
+        
+
+# } Driver Code Ends
