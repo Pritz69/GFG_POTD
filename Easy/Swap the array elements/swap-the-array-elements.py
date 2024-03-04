ca84dfb8746e26cb2b0e5aaa2999ba971ca11b72
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-03-04 18:38:34 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Swap the array elements/swap-the-array-elements.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+class Solution:
+	def swapElements(self, arr, n):
+	    #Code here
+        for i in range(n-2) :
+            t=arr[i]
+            arr[i]=arr[i+2]
+            arr[i+2]=t
+        return arr
+
+#{ 
+ # Driver Code Starts
+
+#Initial Template for Python 3
+		
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        obj = Solution()
+        obj.swapElements(arr, n);
+        for i in arr:
+            print(i, end = " ")
+        print()
+# } Driver Code Ends
