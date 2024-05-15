8397ed365c2dae09e0491a961d875b9ab79d1af2
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-05-15 12:29:00 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Account Merge/account-merge.py
@@ -0,0 +1,61 @@
+#User function Template for python3
+
+import collections
+class UF:
+    def __init__(self, N):
+        self.parents = list(range(N))
+    def union(self, child, parent):
+        self.parents[self.find(child)] = self.find(parent)
+    def find(self, x):
+        if x != self.parents[x]:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+class Solution:
+    def accountsMerge(self, accounts):
+        # Code here
+        uf = UF(len(accounts))
+        
+        # Creat unions between indexes
+        ownership = {}
+        for i, (_, *emails) in enumerate(accounts):
+            for email in emails:
+                if email in ownership:
+                    uf.union(i, ownership[email])
+                ownership[email] = i
+        
+        # Append emails to correct index
+        ans = collections.defaultdict(list)
+        for email, owner in ownership.items():
+            ans[uf.find(owner)].append(email)
+        
+        return [[accounts[i][0]] + sorted(emails) for i, emails in ans.items()]
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        n = int(input())
+        accounts = []
+        for i in range(n):
+            cntEmails = int(input())
+            nameEmails = input().split()
+            accounts.append(nameEmails)
+        ob = Solution()
+        res = ob.accountsMerge(accounts)
+        res.sort()
+        print('[', end = '')
+        for i in range(len(res)):
+            print('[', end = '')
+            for j in range(len(res[i])):
+                if j != (len(res[i]) - 1):
+                    print(res[i][j], end = ', ')
+                else:
+                    print(res[i][j], end='')
+            if (i != len(res) - 1):
+                print('], ')
+            else:
+                print(']', end = '')
+        print(']')
+# } Driver Code Ends
