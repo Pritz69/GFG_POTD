48a49d1cf3266f7234c7cd41d811c9213dae8629
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-06-16 15:37:59 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Prime Pair with Target Sum/prime-pair-with-target-sum.py
@@ -0,0 +1,57 @@
+
+from typing import List
+
+
+class Solution:
+    def getPrimes(self, n : int) -> List[int]:
+        # code here
+        sieve = [True] * (n + 1)
+        sieve[0] = sieve[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if sieve[i]:
+                for j in range(i * i, n + 1, i):
+                    sieve[j] = False
+        
+        
+        # Find two primes that sum to n
+        for i in range(2,n+1) :
+            if sieve[i] and sieve[n-i] :
+                return [i,n-i]
+        
+        return [-1, -1]
+
+
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  #array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        n = int(input())
+
+        obj = Solution()
+        res = obj.getPrimes(n)
+
+        IntArray().Print(res)
+
+# } Driver Code Ends
