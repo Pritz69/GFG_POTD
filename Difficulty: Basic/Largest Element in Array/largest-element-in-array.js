abfefbd528560245583a3ced90af7e42998ce578
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-07-03 00:20:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.js
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function printList(res,n){
+    let s="";
+    for(let i=0;i<n;i++){
+        s+=res[i];
+        s+=" ";
+    }
+    console.log(s);
+}
+
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++){
+            arr[i] = input_ar1[i];
+        }
+        let obj = new Solution();
+        let res = obj.largest(arr, n);
+        console.log(res);
+        
+    }
+}// } Driver Code Ends
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+class Solution {
+    largest(arr, n) {
+        // write your code here
+        let m=0;
+        for(let i=0;i<arr.length;i++)
+        {
+            if (arr[i]>m)
+            {
+                m=arr[i];
+            }
+        }
+        return m;
+    }
+}
