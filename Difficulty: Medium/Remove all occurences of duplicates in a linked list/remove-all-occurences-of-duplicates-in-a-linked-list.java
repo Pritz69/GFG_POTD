600c612416d864da0c5706ade9a6342532c1721d
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-07-03 12:13:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove all occurences of duplicates in a linked list/remove-all-occurences-of-duplicates-in-a-linked-list.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class GFG {
+    Node head;
+    Node tail;
+
+    public void addToTheLast(Node node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Drier program to test above functions */
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int N = sc.nextInt();
+            GFG llist = new GFG();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < N; i++) {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+
+            Solution ob = new Solution();
+            head = ob.removeAllDuplicates(llist.head);
+            llist.printList(head);
+
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public Node removeAllDuplicates(Node head) {
+        // code here
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node prev = dummy;
+        Node curr = head;
+        
+        while (curr != null) {
+            
+            while (curr.next != null && curr.data == curr.next.data) {
+                curr = curr.next;
+            }
+            
+            if (prev.next==curr)
+            {
+                prev=prev.next;
+            }
+            else
+            {
+                prev.next=curr.next;
+            }
+            curr = curr.next;
+        }
+        
+        return dummy.next;
+    }
+}
