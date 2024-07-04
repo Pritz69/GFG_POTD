97994cf90e1af9a716355117646b3a6e9d9c8871
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-07-05 00:28:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Two Repeated Elements/two-repeated-elements.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG
+ {
+	public static void main (String[] args) throws IOException
+	 {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int t = Integer.parseInt(br.readLine());
+	    for(int i=0;i<t;i++){
+	        int n = Integer.parseInt(br.readLine());
+	        String l = br.readLine();
+    		String[] sarr = l.split(" ");
+    		int[] arr = new int[sarr.length];
+    		for(int i1=0;i1<arr.length;i1++){
+    			arr[i1] = Integer.parseInt(sarr[i1]);
+    		}
+    		
+            Solution obj = new Solution();
+            
+            int[] res = obj.twoRepeated(arr, n);
+            System.out.println(res[0] + " " + res[1]);
+	    }
+	}
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find two repeated elements.
+    public int[] twoRepeated(int arr[], int n)
+    {
+        // Your code here
+        Map<Integer,Integer>mp=new HashMap<>();
+        int a[]=new int[2];
+        int i=0;
+        for(int x:arr)
+        {
+            mp.put(x,mp.getOrDefault(x,0)+1);
+            if (mp.get(x)==2)
+            {
+                a[i++]=x;
+            }
+        }
+        return a;
+    }
+    
+}
