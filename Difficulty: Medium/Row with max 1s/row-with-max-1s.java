56d737eef4eb76cec2207d6db3211e776ba845bb
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-07-29 02:28:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int m = Integer.parseInt(inputLine[1]);
+            int[][] arr = new int[n][m];
+            inputLine = br.readLine().trim().split(" ");
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] = Integer.parseInt(inputLine[i * m + j]);
+                }
+            }
+            int ans = new Solution().rowWithMax1s(arr);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int rowWithMax1s(int arr[][]) {
+        // code here
+        int ans=-1;
+        int in=0;
+        int ma=Integer.MIN_VALUE;
+        int s;
+        for(int a[]:arr)
+        {
+            s=0;
+            for(int i=0;i<a.length;i++)
+            {
+                s += a[i];
+            }
+            if(s!=0 && s > ma)
+            {
+                ans=in;
+                ma=s;
+            }
+            in +=1;
+        }
+        return ans;
+    }
+}
