b2cc7a319d1fb30d63f0e638da61229e53f3fdfb
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-12 02:35:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.py
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.py
@@ -1,44 +1,30 @@
 #User function Template for python3
 
 class Solution:
-    def kthElement(self, k, array1, array2):
-        index1 = index2 = mergedIndex = currentElement = 0
-        length1 = len(array1)
-        length2 = len(array2)
-       
-        # Traverse both arrays and merge elements until we reach the k-th position
-        while index1 < length1 and index2 < length2:
-            if mergedIndex == k:
-                return currentElement
-           
-            if array1[index1] < array2[index2]:
-                currentElement = array1[index1]
-                index1 += 1
-            else:
-                currentElement = array2[index2]
-                index2 += 1
-           
-            mergedIndex += 1
-       
-        # If there are remaining elements in array1
-        while index1 < length1:
-            if mergedIndex == k:
-                return currentElement
-           
-            currentElement = array1[index1]
-            index1 += 1
-            mergedIndex += 1
-       
-        # If there are remaining elements in array2
-        while index2 < length2:
-            if mergedIndex == k:
-                return currentElement
-           
-            currentElement = array2[index2]
-            index2 += 1
-            mergedIndex += 1
-       
-        return currentElement
+    def kthElement(self, k, arr1, arr2):
+        
+        n=len(arr1)
+        m=len(arr2)
+        if m < n :
+            return self.kthElement(k,arr2,arr1)
+        
+        left=k
+        low=max(0,k-m)
+        high=min(k,n)
+        while low <= high :
+            mid1=(low+high)>>1
+            mid2=left-mid1
+            l1 = arr1[mid1 - 1] if mid1 > 0 else float('-inf')
+            l2 = arr2[mid2 - 1] if mid2 > 0 else float('-inf')
+            r1 = arr1[mid1] if mid1 < n else float('inf')
+            r2 = arr2[mid2] if mid2 < m else float('inf')
+            if l1 <= r2 and l2 <= r1 :
+                return max(l1,l2)
+            elif l1 > r2 :
+                high=mid1-1
+            else :
+                low=mid1+1
+        return 0
 
 
 #{ 
