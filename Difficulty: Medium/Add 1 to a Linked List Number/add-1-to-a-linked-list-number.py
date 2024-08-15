aa34cc3989ab5ee1c9b16d8d392db4b7724e0e0e
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-15 11:42:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.py
@@ -0,0 +1,111 @@
+#User function Template for python3
+
+'''
+
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def addOne(self,head):
+        #Returns new head of linked List.
+        def rev(node) :
+            prev=None
+            curr=node
+            while curr :
+                nxt=curr.next
+                curr.next=prev
+                prev=curr
+                curr=nxt
+            return prev
+        
+        head = rev(head)
+        curr = head
+        carry = 1  
+        
+        while curr:
+            sum_val = curr.data + carry
+            curr.data = sum_val % 10
+            carry = sum_val // 10
+            
+            prev = curr
+            curr = curr.next
+        
+        if carry > 0:
+            prev.next = Node(carry)
+        
+        return rev(head)
+            
+            
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+# Node Class
+class Node:
+
+    def __init__(self, data):  # data -> value stored in node
+        self.data = data
+        self.next = None
+
+
+# Linked List Class
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def insert(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(value)
+            self.tail = self.tail.next
+        self.size += 1
+
+
+def PrintList(head):
+    while head:
+        print(head.data, end='')
+        head = head.next
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+
+        list1 = LinkedList()
+        arr = list(map(int, input().strip().split()))
+        first = arr[0]
+        for i in arr:
+            list1.insert(i)
+        n1 = list1.size
+        resHead = Solution().addOne(list1.head)
+
+        n2 = 0
+        current = resHead
+        while current is not None:
+            current = current.next
+            n2 += 1
+        if n2 == 1:
+            if n1 > 1:
+                print("Return the modified linkedlist")
+            if n1 == 1:
+                if first < 9:
+                    PrintList(resHead)
+                    print()
+                else:
+                    print("Return the modified linkedlist")
+        else:
+            PrintList(resHead)
+            print()
+
+# } Driver Code Ends
