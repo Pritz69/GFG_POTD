cc04dab95792a3b5678511b9d9a35b2ac0fb127f
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-09-12 20:20:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Middle of a Linked List/middle-of-a-linked-list.py
@@ -0,0 +1,73 @@
+# your task is to complete this function
+
+'''
+class node:
+    def __init__(data):
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    #  Should return data of middle node. If linked list is empty, then  -1
+    def getMiddle(self, head):
+        # Code here
+        # return the value stored in the middle node
+        s,f=head,head
+        while f and f.next :
+            s=s.next
+            f=f.next.next
+        return s.data
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python3
+
+
+# Node Class
+class node:
+
+    def __init__(self):
+        self.data = None
+        self.next = None
+
+
+# Linked List Class
+class Linked_List:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, data):
+        if self.head == None:
+            self.head = node()
+            self.tail = self.head
+            self.head.data = data
+        else:
+            new_node = node()
+            new_node.data = data
+            new_node.next = None
+            self.tail.next = new_node
+            self.tail = self.tail.next
+
+
+def printlist(head):
+    temp = head
+    while temp is not None:
+        print(temp.data, end=" ")
+        temp = temp.next
+    print('')
+
+
+# Driver Program
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        list1 = Linked_List()
+        arr = list(map(int, input().strip().split()))
+        for i in arr:
+            list1.insert(i)
+        ob = Solution()
+        print(ob.getMiddle(list1.head))
+
+# } Driver Code Ends
