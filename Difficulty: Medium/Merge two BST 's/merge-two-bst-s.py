bbf79e27c48b75f810fca96c2f68e5c803c32a3c
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-09-30 00:25:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.py
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.py
@@ -5,39 +5,20 @@
 # } Driver Code Ends
 #User function Template for python3
 class Solution:
-    def merge(self, root1, root2):
-        # code here
-        def inorder(node,l) :
-            if not node :
-                return
-            inorder(node.left,l)
+    def inorder(self,root):
+        l=[]
+        def helper(node):
+            if node is None:
+                return None
+            helper(node.left)
             l.append(node.data)
-            inorder(node.right,l)
+            helper(node.right)
             return l
-        l1=inorder(root1,[])
-        l2=inorder(root2,[])
-        i=0
-        j=0
-        ans=[]
-        while i<len(l1) and j<len(l2) :
-            if l1[i] < l2[j] :
-                ans.append(l1[i])
-                i +=1
-            elif l2[j] < l1[i] :
-                ans.append(l2[j])
-                j +=1
-            else :
-                ans.append(l1[i])
-                ans.append(l2[j])
-                i +=1
-                j +=1
-        while i <len(l1) :
-            ans.append(l1[i])
-            i +=1
-        while j < len(l2) :
-            ans.append(l2[j])
-            j +=1
-        return ans
+        return helper(root)
+    def merge(self, root1, root2):
+        final=self.inorder(root1)+self.inorder(root2)
+        final.sort()
+        return final
 
 #{ 
  # Driver Code Starts.
