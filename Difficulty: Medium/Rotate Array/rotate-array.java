b5022f815cc2e030301e77838a975bf76d4c3d52
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-11-18 19:49:38 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0) {
+            String line = in.readLine();
+            String[] tokens = line.split(" ");
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int d = Integer.parseInt(in.readLine().trim()); // rotation count (key)
+            int n = array.size();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) arr[i] = array.get(i);
+
+            new Solution().rotateArr(arr, d); // rotating the array
+            StringBuilder sb = new StringBuilder();
+
+            // printing the elements of the array
+            for (int value : arr) sb.append(value).append(" ");
+            out.println(sb.toString().trim());
+
+            out.println("~");
+        }
+        out.flush();
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    
+    static void reverse(int start, int end, int arr[]){
+        while(start<=end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    static void rotateArr(int arr[], int d) {
+        // add your code here
+        int m = d%arr.length;
+            reverse(0,m-1,arr);
+            reverse(m,arr.length-1,arr);
+            reverse(0,arr.length-1,arr);
+        }
+}
+
