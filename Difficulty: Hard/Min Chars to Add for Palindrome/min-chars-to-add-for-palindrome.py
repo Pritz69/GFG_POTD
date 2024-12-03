07b4f1303384dd928554f7b479722d3634d70ae4
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-12-03 23:48:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Min Chars to Add for Palindrome/min-chars-to-add-for-palindrome.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minChar(self, s):
+        rev_s = s[::-1]
+        combined = s + "#" + rev_s
+        n = len(combined)
+        lps = [0] * n
+        
+        for i in range(1, n):
+            j = lps[i - 1]
+            while j > 0 and combined[i] != combined[j]:
+                j = lps[j - 1]
+            if combined[i] == combined[j]:
+                j += 1
+            lps[i] = j
+        
+        return len(s) - lps[-1]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        s = input()
+        obj = Solution()
+        ans = obj.minChar(s)
+        print(ans)
+        print("~")
+
+# } Driver Code Ends
