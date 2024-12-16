c689d99644b00e081de2adc97a644b076873cf9e
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-12-16 13:19:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.py
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.py
@@ -1,30 +1,28 @@
 #User function Template for python3
 
+
 class Solution:
-    def kthElement(self, k, arr1, arr2):
-        
-        n=len(arr1)
-        m=len(arr2)
-        if m < n :
-            return self.kthElement(k,arr2,arr1)
-        
-        left=k
-        low=max(0,k-m)
-        high=min(k,n)
-        while low <= high :
-            mid1=(low+high)>>1
-            mid2=left-mid1
-            l1 = arr1[mid1 - 1] if mid1 > 0 else float('-inf')
-            l2 = arr2[mid2 - 1] if mid2 > 0 else float('-inf')
-            r1 = arr1[mid1] if mid1 < n else float('inf')
-            r2 = arr2[mid2] if mid2 < m else float('inf')
-            if l1 <= r2 and l2 <= r1 :
-                return max(l1,l2)
-            elif l1 > r2 :
-                high=mid1-1
+
+    def kthElement(self, a, b, k):
+        i,j=0,0
+        n=len(a)
+        m=len(b)
+        ans=[]
+        while i<n and j<m :
+            if a[i] <= b[j] :
+                ans.append(a[i])
+                i +=1
             else :
-                low=mid1+1
-        return 0
+                ans.append(b[j])
+                j +=1
+        while i <n :
+            ans.append(a[i])
+            i +=1
+        while j < m :
+            ans.append(b[j])
+            j +=1
+        return ans[k-1]
+
 
 
 #{ 
@@ -42,8 +40,8 @@ def main():
         a = [int(x) for x in input().strip().split()]
         b = [int(x) for x in input().strip().split()]
         ob = Solution()
-        print(ob.kthElement(k, a, b))
-
+        print(ob.kthElement(a, b, k))
+        print("~")
         T -= 1
 
 
