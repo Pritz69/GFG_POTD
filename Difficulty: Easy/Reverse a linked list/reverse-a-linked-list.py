a693ec2e565f73eb67018d9a56329381c67e88e0
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-01-18 21:06:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.py
@@ -0,0 +1,74 @@
+#function Template for python3
+
+"""
+# Node Class
+
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+"""
+
+class Solution:
+    def reverseList(self, head):
+        # Code here
+        p=None
+        c=head
+        while c :
+            n=c.next
+            c.next=p
+            p=c
+            c=n
+        return p
+
+
+
+#{ 
+ # Driver Code Starts
+# Node Class
+class Node:
+
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+
+# Linked List Class
+class Linked_List:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, val):
+        if self.head is None:
+            self.head = Node(val)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(val)
+            self.tail = self.tail.next
+
+
+def printList(head):
+    tmp = head
+    while tmp:
+        print(tmp.data, end=' ')
+        tmp = tmp.next
+    print()
+
+
+if __name__ == '__main__':
+    for i in range(int(input())):
+
+        arr = [int(x) for x in input().split()]
+
+        lis = Linked_List()
+        for i in arr:
+            lis.insert(i)
+
+        newHead = Solution().reverseList(lis.head)
+        printList(newHead)
+        print("~")
+
+# } Driver Code Ends
