bb2c21986df5977b0732417c1b34ae8a97f249a9
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-02-11 11:58:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check for BST/check-for-bst.py
+++ Difficulty: Easy/Check for BST/check-for-bst.py
@@ -1,27 +1,20 @@
 #User function Template for python3
 
-import sys
-sys.setrecursionlimit(10**6)
 
 class Solution:
     
     #Function to check whether a Binary Tree is BST or not.
     def isBST(self, root):
         #code here
-        def check(node) :
+        a=[]
+        def func(node) :
             if not node :
-                return [True,float('inf'),float('-inf')]
-            if not node.left and not node.right :
-                return [True,node.data,node.data]
-            l=check(node.left)
-            r=check(node.right)
-            if l[0] and r[0] and l[2] < node.data < r[1] :
-                return [True,min(l[1],node.data),max(r[2],node.data)]
-            else :
-                return [False,float('inf'),float('-inf')]
-        
-        return check(root)[0]
- 
+                return None
+            func(node.left)
+            a.append(node.data)
+            func(node.right)
+        func(root)
+        return a==sorted(a)
 
 
 #{ 
@@ -106,5 +99,6 @@ if __name__ == "__main__":
             print("true")
         else:
             print("false")
+        print("~")
 
 # } Driver Code Ends
