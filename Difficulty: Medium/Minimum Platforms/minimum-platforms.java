4783a1347eec55bfd46cfe134fe970cbd8e8163f
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-03-27 11:57:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            // First array input (arr)
+            String[] str1 = br.readLine().trim().split(
+                " "); // Read the first line and split by spaces
+            int n = str1.length;
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] =
+                    Integer.parseInt(str1[i]); // Convert each element to an integer
+            }
+
+            // Second array input (dep)
+            String[] str2 = br.readLine().trim().split(
+                " "); // Read the second line and split by spaces
+            int m = str2.length;
+            int[] dep = new int[m];
+            for (int i = 0; i < m; i++) {
+                dep[i] =
+                    Integer.parseInt(str2[i]); // Convert each element to an integer
+            }
+
+            Solution obj = new Solution();
+            System.out.println(obj.findPlatform(arr, dep));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[]) {
+        // add your code here
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int count = 0;  
+        int maxCount = 1;
+        int j=0;
+        int i=0;
+        while (i < arr.length)
+        {
+            if (arr[i] <= dep[j]) {
+                count++;
+                i++;
+            } else 
+            {
+                count--;
+                j++;
+            }
+            maxCount = Math.max(maxCount, count);
+        }
+        return maxCount;
+    }
+}
