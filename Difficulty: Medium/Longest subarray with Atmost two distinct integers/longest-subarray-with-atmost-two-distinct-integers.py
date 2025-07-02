925ee578cb16d16e2ff2d55a374ef5a433e867f8
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-07-02 12:13:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def totalElements(self, s):
+        # Code here
+        ans=0
+        l=0
+        r=0
+        n=len(s)
+        d={}
+        while r < n :
+            c=s[r]
+            d[s[r]]=d.get(s[r],0)+1
+            while len(d) > 2 :
+                d[s[l]]-=1
+                if d[s[l]]==0 :
+                    d.pop(s[l])
+                l +=1
+            if len(d)<=2:
+                ans =max(ans,(r-l)+1)
+            r +=1
+        return ans
