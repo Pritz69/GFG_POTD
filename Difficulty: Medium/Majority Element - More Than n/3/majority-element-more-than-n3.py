66a510145d3d9f8b38f432d267b73168c30eae6c
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-07-26 15:38:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element - More Than n/3/majority-element-more-than-n3.py
@@ -0,0 +1,41 @@
+class Solution:
+    # Function to find the majority elements in the array
+    def findMajority(self, nums):
+       
+        candidate1, candidate2, count1, count2 = None, None, 0, 0
+
+        # First pass: Find potential candidates
+        for num in nums:
+            if candidate1 == num:
+                count1 += 1
+            elif candidate2 == num:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+
+
+        count1, count2 = 0, 0
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+
+
+        result = []
+        n = len(nums)
+        if count1 > n // 3:
+            result.append(candidate1)
+        if count2 > n // 3:
+            result.append(candidate2)
+
+        return sorted(result) if result else []
+
+
+
+
