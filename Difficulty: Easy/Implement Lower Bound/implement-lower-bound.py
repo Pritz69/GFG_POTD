96ff4abf13d7576513be1882ca7eb1031fb39f32
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-07-27 22:22:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lowerBound(self, arr, target):
+        # code here
+        n=len(arr)
+        l=0
+        h=n-1
+        ans=n
+        while l <=h :
+            m=l+(h-l)//2
+            if arr[m] >= target :
+                ans=m
+                h=m-1
+            else :   
+                l=m+1
+        return ans
+        
