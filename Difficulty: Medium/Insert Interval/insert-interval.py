12ef6d984f9787a5610ffed52ba1642425aadd0c
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-08-15 17:43:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Insert Interval/insert-interval.py
+++ Difficulty: Medium/Insert Interval/insert-interval.py
@@ -1,8 +1,3 @@
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-# } Driver Code Ends
 #User function Template for python3
 
 class Solution:
@@ -17,24 +12,3 @@ class Solution:
             else :
                 ans[-1][1]=max(ans[-1][1],x[1])
         return ans
-
-#{ 
- # Driver Code Starts.
-if __name__ == '__main__': 
-    t = int(input ())
-    for _ in range (t):
-        N = int(input())
-        intervals = [list(map(int, input().split())) for i in range(N)]
-        newEvent = list(map(int, input().split()))
-        ob = Solution()
-        res = ob.insertInterval(intervals, newEvent)
-        print('[', end = '')
-        for i in range(len(res)):
-            print('[', end = '')
-            print(str(res[i][0])+','+str(res[i][1]), end = '')
-            print(']', end = '')
-            if i < len(res)-1:
-                print(',', end='')
-        print(']')
-        print("~")
-# } Driver Code Ends
