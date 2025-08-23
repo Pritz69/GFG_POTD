b6b76617fa501a53e555a867c1f849f6bbc69276
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-08-24 00:40:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.py
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.py
@@ -1,51 +1,24 @@
-def isF(arr,k,mid,n):
-    count = 1
-    m_sum = 0
-    for i in range(n):
-        
-        if m_sum + arr[i] > mid:
-            count += 1
-            m_sum = arr[i]
-        else:
-            m_sum += arr[i]
-    return count <= k        
-
 class Solution:
+    def isPossible(self,arr,val,k):
+        count,curr=1,0
+        for pages in arr:
+            curr+=pages
+            if curr>val:
+                curr=pages
+                count+=1
+            if count>k or pages>val:
+                return False
+        return True
     
-    def findPages(self,arr, k):
-        n = len(arr)
-        if k > n:
+    def findPages(self, arr, k):
+        if len(arr)<k:
             return -1
-        high = sum(arr)
-        low = max(arr)
-        
-        while low <= high:
-            mid = (low+high)//2
-            
-            if isF(arr,k,mid,n):
-                high = mid - 1
-                res = mid
+        l,h,ans=0,sum(arr),-1
+        while l<=h:
+            mid=(l+h)//2
+            if self.isPossible(arr,mid,k):
+                ans=mid
+                h=mid-1
             else:
-                low = mid + 1
-        return res
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-import bisect
-#Main
-if __name__ == '__main__':
-    t = int(input())
-    while t:
-        t -= 1
-        A = [int(x) for x in input().strip().split()]
-        nd = [int(x) for x in input().strip().split()]
-        D = nd[0]
-        ob = Solution()
-        ans = ob.findPages(A, D)
-        print(ans)
-        print("~")
-
-# } Driver Code Ends
+                l=mid+1
+        return ans
