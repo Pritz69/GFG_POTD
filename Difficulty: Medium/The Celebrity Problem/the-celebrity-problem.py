f21c375103528fe9a2bd1b1b1d5d466ac1847bfc
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-08-30 21:40:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.py
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.py
@@ -2,31 +2,28 @@ from collections import defaultdict
 class Solution:
     def celebrity(self, mat):
         # code here
-        d=defaultdict(list)
-        for i in range(len(mat)) :
-            for j in range(len(mat)) :
-                if mat[i][j]==1 :
-                    d[j].append(i)
-        for i in range(len(mat)) :
-            c0=0
-            for j in range(len(mat)) :
-                if mat[i][j]==0 :
-                    c0 +=1
-            if c0==len(mat) and len(d[i])==len(mat)-1 :
-                return i
-        return -1
-#{ 
- # Driver Code Starts
-# Main function to handle input and output
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        n = int(input())
-        M = []
-        for _ in range(n):
-            M.append(list(map(int, input().split())))
-
-        ob = Solution()
-        print(ob.celebrity(M))
-
-# } Driver Code Ends
+        d=defaultdict(set)
+        n=len(mat)
+        m=len(mat[0])
+        for i in range(n) :
+            for j in range(m) :
+                v=mat[i][j]
+                if v==1 :
+                    d[i].add(j)
+        #print(d)
+        l=d.values()
+        #print(l)
+        ans=-1
+        for x in d :
+            #print(x,d[x],len(d[x]))
+            if len(d[x])==1 :
+                l=d.values()
+                c=0
+                for e in l :
+                    #print(e)
+                    if x in e :
+                        c +=1
+                if c==n:
+                    ans=x
+        return ans
+        
