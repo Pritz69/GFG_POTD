339bfb8a86bab28c4dfae4dda7b5b1af24213c50
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-08-31 22:23:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.py
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.py
@@ -14,26 +14,3 @@ class Solution:
                 r -=1
                 
         return ans
-#{ 
- # Driver Code Starts
-#Initial template for Python 3
-
-import math
-
-
-def main():
-    t = int(input())
-    while (t > 0):
-
-        arr = [int(x) for x in input().strip().split()]
-        obj = Solution()
-        print(obj.maxWater(arr))
-
-        t -= 1
-        print("~")
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
