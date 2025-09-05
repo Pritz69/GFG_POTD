40051d2e67e458acbcc21913c40fa9aa9a4e439c
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-09-05 21:12:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.py
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.py
@@ -1,13 +1,9 @@
 '''
-	Function Arguments: head of the original list.
-	Return Type: head of the new list formed.
-	{
-		# Node Class
-		class Node:
-		    def __init__(self, data):   # data -> value stored in node
-		        self.data = data
-		        self.next = None
-	}'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
 	
 class Solution:
     def segregate(self, head):
@@ -15,64 +11,25 @@ class Solution:
         c0,c1,c2=0,0,0
         c=head
         while c :
-            if c.data==0:
+            if c.data==0 :
                 c0 +=1
             elif c.data==1 :
                 c1 +=1
             else :
                 c2 +=1
             c=c.next
-        ans=Node(-1)
-        new=ans
-        while c0 :
-            n=Node(0)
-            ans.next=n
-            ans=ans.next
-            c0 -=1
-        while c1 :
-            n=Node(1)
-            ans.next=n
-            ans=ans.next
-            c1 -=1
-        while c2 :
-            n=Node(2)
-            ans.next=n
-            ans=ans.next
-            c2 -=1
+        c=head
+        while c :
+            if c0 > 0 :
+                c.data=0
+                c0 -=1
+            elif c1 > 0 :
+                c.data=1
+                c1 -=1
+            else :
+                c.data=2
+                c2 -=1
+            c=c.next 
         
-        return new.next
-#{ 
- # Driver Code Starts
-# Initial Template for Python 3
-
-
-class Node:
-
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
-def printList(node):
-    while node:
-        print(node.data, end=" ")
-        node = node.next
-    print()
-
-
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-        arr = list(map(int, input().strip().split()))
-        head = None
-        if arr:
-            head = Node(arr[0])
-            tail = head
-            for value in arr[1:]:
-                tail.next = Node(value)
-                tail = tail.next
-
-        printList(Solution().segregate(head))
-        print("~")
-
-# } Driver Code Ends
+        return head
+    
