110e7293fb35b37416fdee72080baf778ec5958b
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2026-01-13 22:29:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.py
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.py
@@ -1,60 +1,31 @@
-
 class Solution:
     def isBalanced(self, s):
         # code here
         if len(s)==1 :
             return False
-        st=[]
+        
+        stack=[]
+        
         for x in s :
             if x==")" :
-                if st and st[-1]=="(" :
-                    st.pop()
+                if stack and stack[-1]=="(" :
+                    stack.pop()
                 else :
-                    st.append(x)
+                    stack.append(x)
             elif x=="}" :
-                if st and st[-1]=="{" :
-                    st.pop()
+                if stack and stack[-1]=="{" :
+                    stack.pop()
                 else :
-                    st.append(x)
+                    stack.append(x)
             elif x=="]" :
-                if st and st[-1]=="[" :
-                    st.pop()
+                if stack and stack[-1]=="[" :
+                    stack.pop()
                 else :
-                    st.append(x)
+                    stack.append(x)
             else :
-                st.append(x)
-        return st==[]
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-import atexit
-import io
-import sys
-
-#Contributed by : Nagendra Jha
-
-_INPUT_LINES = sys.stdin.read().splitlines()
-input = iter(_INPUT_LINES).__next__
-_OUTPUT_BUFFER = io.StringIO()
-sys.stdout = _OUTPUT_BUFFER
-
-
-@atexit.register
-def write():
-    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
-
-
-if __name__ == '__main__':
-    test_cases = int(input())
-    for cases in range(test_cases):
-        s = str(input())
-        obj = Solution()
-        if obj.isBalanced(s):
-            print("true")
-        else:
-            print("false")
-        print("~")
-
-# } Driver Code Ends
+                stack.append(x)
+        
+        if stack==[] :
+            return True
+        else :
+            return False
